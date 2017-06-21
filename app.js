ff0dd027cb20f1b5352f980493c35ffde33fe42e
Kenji Hirabayashi <mkhira2@gmail.com>
2017-06-21 12:14:13 -0500

    added form validation

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 const mustache = require('mustache-express')
 const bodyParser = require('body-parser')
+const expressValidator = require('express-validator')
 
 app.engine('mustache', mustache())
 app.set('view engine', 'mustache')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(expressValidator())
 
 app.listen(3000, function() {
   console.log("ok cool, listening!")
@@ -25,14 +27,36 @@ app.get('/contact/new', function(request, response) {
 })
 
 app.post('/contact', function(request, response) {
+
+// grabs the field by its name from the request
+
   const name = request.body.name
   const email = request.body.email
   const message = request.body.message
 
-  response.render('contact-thanks', {
-    pageTitle: 'Thanks!',
-    name: name,
-    email: email,
-    message: message
-  })
+// Step 1: validate the data
+  request.checkBody("name", "You must enter your name!").notEmpty()
+  request.checkBody("email", "You must enter your email!").notEmpty()
+  request.checkBody("message", "You must enter a message!").notEmpty()
+
+// Step 2: check if there were any errors
+  var errors = request.validationErrors()
+
+  if (errors) {
+    response.render('contact-form', {
+      pageTitle: "New Contact",
+        errors: errors,
+        name: name,
+        email: email,
+        message: message
+    })
+  } else {
+
+    response.render('contact-thanks', {
+      pageTitle: 'Thanks!',
+      name: name,
+      email: email,
+      message: message
+    })
+  }
 })
